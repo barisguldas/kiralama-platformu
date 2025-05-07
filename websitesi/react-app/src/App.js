15b2a2d0c10e4497f51ee9d3f683d79da2d25ad7
LazmodBerk <149705330+LazmodBerk@users.noreply.github.com>
2025-05-07 16:51:47 +0300

    Hatalar giderildi

--- websitesi/react-app/src/App.js
+++ websitesi/react-app/src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import AnaSayfa from './AnaSayfa';
 import GirisPaneli from './GirisPaneli';
 import KayitPaneli from './KayitPaneli';
 import IlanOlusturma from './ilanolusturma';
 import Profil from './ProfilSayfa'; // Profil sayfasının doğru yolu
 import KullaniciProfil from './KullaniciProfil';
+import IlanArayuzu from './IlanArayuzu';
 
 function App() {
   return (
     <Router>
-      <div>
-        {/* Sayfa içeriği */}
-        <Routes>
-          <Route path="/" element={<Navigate to="/anasayfa" />} />
-          <Route path="/anasayfa" element={<AnaSayfa />} />
-          <Route path="/giris" element={<GirisPaneli />} />
-          <Route path="/kayit" element={<KayitPaneli />} />
-          <Route path="/ilanolustur" element={<IlanOlusturma />} />
-          <Route path="/profil" element={<Profil />} /> {/* Profil sayfası */}
-          <Route path="/kullaniciprofil" element={<KullaniciProfil />} /> 
-        </Routes>
-      </div>
+      <Routes>
+        {/* "/" adresine girince otomatik olarak "/anasayfa"ya yönlendir */}
+        <Route path="/" element={<Navigate to="/anasayfa" />} />
+        <Route path="/anasayfa" element={<AnaSayfa />} />
+        <Route path="/giris" element={<GirisPaneli />} />
+        <Route path="/kayit" element={<KayitPaneli />} />
+        <Route path="/ilanolustur" element={<IlanOlusturma/>} />
+        <Route path="/ilanlar/:ilanid/:baslik" element={<IlanArayuzu />} />
+        <Route path="/profil" element={<Profil />} /> {/* Profil sayfası */}
+        <Route path="/kullaniciprofil" element={<KullaniciProfil />} /> 
+      </Routes>
     </Router>
   );
 }
